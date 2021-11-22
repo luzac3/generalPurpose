bb50e4dbb8b897cff970c37ef96cb7b713dc3685
osugi <oosugi-t@vehiclevision.jp>
2021-11-22 12:06:35 +0900

    nodeModuleの追加

---
+++ teaParty/js/index.js
@@ -0,0 +1,16 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./ts/index.ts ***!
+  \*********************/
+
+$(function () {
+    $(".hashTag").on("click", function () {
+        window.open("https://twitter.com/intent/tweet?hashtags=けもふれお茶会");
+    });
+});
+
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
